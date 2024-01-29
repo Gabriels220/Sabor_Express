73da8c56b750a3d7dc37d92ff86eb0d58dc7cd70
Gabriel Mendes <bielmas220@gmil.com>
2024-01-29 14:11:46 -0300

    Tirando o print(response)

--- Projeto Sabor express/app.py
+++ Projeto Sabor express/app.py
@@ -5,8 +5,6 @@ url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
 response = requests.get(url)
 
-print(response)
-
 if response.status_code == 200:
     dados_json = response.json()
     dados_restaurante = {}
