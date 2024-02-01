50b58fb126f7a921afde402b4139d7d477cd237e
Gabriel Mendes <bielmas220@gmil.com>
2024-02-01 14:11:11 -0300

    Acrescentando o FastAPI

--- Projeto Sabor express/app.py
+++ Projeto Sabor express/app.py
@@ -5,6 +5,8 @@ url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
 response = requests.get(url)
 
+print(response)
+
 if response.status_code == 200:
     dados_json = response.json()
     dados_restaurante = {}
